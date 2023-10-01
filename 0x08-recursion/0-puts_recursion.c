de62afe32c940760666f974bd1b99c2b03d27c0f
kodokingu <i.nwajohn@gmail.com>
2023-10-01 12:21:56 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * This puts recursion
+ * /
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+ 
---
+++ 0x08-recursion/_putcher.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * bla bla bla
+ * /
+int _putcher(char c)
+{
+	retune (write(1, &c. 1));
+}
