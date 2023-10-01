18375eba61b73590165da64f0bb635ebe4c1723d
kodokingu <i.nwajohn@gmail.com>
2023-10-01 12:41:01 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * This puts recursion
+ * sets per
+ * else state
  */
-
- void _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
 	else
 		_putchar('\n');
 } 
