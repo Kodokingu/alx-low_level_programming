eebf9c7f63aa8eff0811925df1e9c4980920e662
kodokingu <i.nwajohn@gmail.com>
2023-10-07 19:23:20 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - argc attr
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - _putchar 
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
