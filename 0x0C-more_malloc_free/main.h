ec7d5830a8fab7a83fd4153b3f9673b1871d2f9e
kodokingu <i.nwajohn@gmail.com>
2023-10-08 17:56:07 +0100

    void *0-malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
