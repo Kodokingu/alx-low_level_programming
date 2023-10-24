f97bde26517649201cf3f54109ea809e1e98edb2
kodokingu <i.nwajohn@gmail.com>
2023-10-24 14:33:43 +0100

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * main - listint_len
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
